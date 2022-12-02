f34e56c8ca0fe05dbdbb9258e34f41aff0c95b60
Evan Brown <evanb@viatouchmedia.com>
2022-12-02 14:20:36 -0800

    sim postprocess

---
+++ sim_com/initiate_postprocess.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import pika
+import json
+
+credentials = pika.PlainCredentials('nano','nano')
+parameters = pika.ConnectionParameters('localhost',5672,'/',credentials)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue="cvPost",durable = True)
+
+data = '{\n "transid": "trans702", \n "cv_activities":"TEST-cv_activities"\n "ls_activities":"TEST-ls_activities"\n}'
+mess = json.dumps(data)
+mess =json.loads(mess)
+
+channel.basic_publish(exchange='',
+                        routing_key="cvPost",
+                        body=mess)
+
+print(" [x] Sent data %", data)
+connection.close()
